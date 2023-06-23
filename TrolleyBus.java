6372135010fd7af8b95014baca034d76ad2c6e0c
Bombberman1 <120326810+Bombberman1@users.noreply.github.com>
2023-06-23 20:44:41 +0300

    Refactoring Lab

--- TrolleyBus.java
+++ TrolleyBus.java
@@ -1,15 +1,14 @@
 package ua.lviv.iot.algo.part1.lab1.TrolleyBus;
-import lombok.*;
 
-import java.lang.reflect.Constructor;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
-
-public class TrolleyBus{
-    private int id;
+@NoArgsConstructor
+public class TrolleyBus {
+    private int id = 100;
     private int routeNumber;
     private String currentStop;
     private double maxSpeed;
@@ -17,47 +16,40 @@ public class TrolleyBus{
     private int capacity;
     private int passengers;
 
-    public TrolleyBus(){
-        this.id = 100;
-    }
-
     private static TrolleyBus instance = new TrolleyBus();
+
     private static TrolleyBus getInstance() {
         return instance;
     }
 
-    public void stop(){
+    public void stop() {
         currentSpeed = 0;
     }
-    public void start(){
+
+    public void start() {
         currentSpeed = 20;
     }
-    public void addPassenger(){
-        if(passengers < capacity){
+
+    public void addPassenger() {
+        if (passengers < capacity) {
             passengers++;
         }
     }
-    public void removePassenger(){
-        if(passengers > 0){
+
+    public void removePassenger() {
+        if (passengers > 0) {
             passengers--;
         }
     }
 
     public static void main(String[] args) {
-        TrolleyBus[] bus = new TrolleyBus[7];
-        bus[0] = new TrolleyBus(50, 13, "Lviv", 80, 0, 30, 10);
-        bus[1] = instance.getInstance();
-        bus[2] = new TrolleyBus();
-        for(int i = 0; i < 3; i++){
-            System.out.println(bus[i]);
+        TrolleyBus[] trolleyBuses = {
+                new TrolleyBus(50, 13, "Lviv", 80, 0, 30, 10),
+                instance.getInstance(),
+                instance.getInstance(),
+                new TrolleyBus()};
+        for (var trolleyBus : trolleyBuses) {
+            System.out.println(trolleyBus);
         }
-        bus[0].start();
-        System.out.println(bus[0]);
-        bus[0].addPassenger();
-        System.out.println(bus[0]);
-        bus[0].removePassenger();
-        System.out.println(bus[0]);
-        bus[0].stop();
-        System.out.println(bus[0]);
     }
 }
